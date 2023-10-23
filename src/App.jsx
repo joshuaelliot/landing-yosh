52c22910268acaec75f9378be6234b2374adc30c
joshua <85379582+joshuaelliot@users.noreply.github.com>
2023-10-22 20:45:38 -0600

    modificado logica component App / agregado popup

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,19 @@
 import { useState } from 'react'
 
 import './App.css'
+import ItemCard from "./components/item-card"
 
-const number = 51910550230;
 function App() {
   const [count, setCount] = useState(0)
-
+const lista =[1,2,4,5,76,8,9,5,3,4,6,87,]
   return (
     <>
-     <div class="container">
+     <div className="container">
 
-
-  <div class="item">
-    <figure>
-      <img src="https://i.pinimg.com/564x/b4/bd/ee/b4bdee855a004c9181847482f5e4547d.jpg" 
-      alt="" />
-      </figure>
-    <div className='description-article'>
-      <div className='name-article'>
-        <h3>BOBO</h3>
-        <h3>Mochila estilo coreano</h3>
-        <h3>s/80</h3>
-        <button>pidelo aqui <a href={`https://wa.me/${number}`}>🃏</a></button>
-      </div>
-     
-    </div>
-  </div>
-
-
-
-  <div class="item">2</div>
-  <div class="item">3</div>
-  <div class="item">4</div>
-  <div class="item">5</div>
-  <div class="item">6</div>
-  <div class="item">7</div>
-  <div class="item">8</div>
-  <div class="item">9</div>
-  
-</div>
+      {lista.map((e,index)=>{
+        return <ItemCard key={index} />
+      })}
+     </div>
 
     </>
   )
---
+++ src/components/item-card.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { useState } from "react";
+import LogoWasap from "../assets/whatssapp.svg"
+import LogoClosePopup from "../assets/close-popup.svg"
+
+const number = 51910550230;
+export default function ItemCard(props){
+const [popup,setPopup] = useState(false);
+    const showPopPup=(e)=>{
+        setPopup(true);
+    }
+    const closePopup = ()=>{
+        setPopup(false);
+    }
+    return(
+        <div className="item" >
+        <figure
+        onClick={showPopPup}
+        >
+          <img src="https://i.pinimg.com/564x/b4/bd/ee/b4bdee855a004c9181847482f5e4547d.jpg" 
+          alt="" />
+          </figure>
+        <div className='description-article'>
+          <div className='name-article'>
+            <h3>BOBO</h3>
+            <h3>Mochila estilo coreano</h3>
+            <h3>s/80</h3>
+            <button className='btn-wasap'><a href={`https://wa.me/${number}`}>Pidelo aqui   <img
+            className='logo-wasap' src={LogoWasap} alt="" /></a></button>
+          </div>
+        </div>
+        {popup && (
+            <div className="show-poppup">
+                
+                <button
+                className="btn-close-popup"
+                onClick={closePopup}>
+                    <img src={LogoClosePopup} alt="" />
+                </button>
+                <img src="" alt="" />
+            </div>
+        )}
+      </div>
+    )
+}
