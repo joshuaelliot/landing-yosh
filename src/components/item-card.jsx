20705772f14da3a26b0384e1138bdf045142fec5
joshua <85379582+joshuaelliot@users.noreply.github.com>
2023-10-28 17:57:59 -0600

    se modifica size description article,color,font-size,font-family

--- src/components/item-card.jsx
+++ src/components/item-card.jsx
@@ -5,7 +5,7 @@ import LogoClosePopup from "../assets/close-popup.svg"
 import {Carousel} from "./slider-popup"
 const number = 51910550230;
 
-
+let precio = 60;
 
 export default function ItemCard(props){
 const [popup,setPopup] = useState(false);
@@ -23,14 +23,14 @@ const [popup,setPopup] = useState(false);
           <img src="https://i.pinimg.com/564x/b4/bd/ee/b4bdee855a004c9181847482f5e4547d.jpg" 
           alt="" />
           </figure>
-        <div className='description-article'>
-          <div className='name-article'>
-            <h3>BOBO</h3>
-            <h3>Mochila estilo coreano</h3>
-            <h3>s/80</h3>
-            <button className='btn-wasap'><a href={`https://wa.me/${number}`}>Pidelo aqui   <img
-            className='logo-wasap' src={LogoWasap} alt="" /></a></button>
+        <div className='description-article-container'>
+          <div className='data-article'>
+            <h3 className="marca-article">BOBO</h3>
+            <h3 className="description">Mochila estilo coreano</h3>
+            <h3 className="price-article">{`s/${precio}`}</h3>
           </div>
+          <button className='btn-wasap'><a href={`https://wa.me/${number}`}>Pidelo aqui   <img
+            className='logo-wasap' src={LogoWasap} alt="" /></a></button>
         </div>
         {popup && (
             <div className="show-poppup">
