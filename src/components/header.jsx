746cf4079cd0faa80ef601ca38111756e9795433
joshua <85379582+joshuaelliot@users.noreply.github.com>
2023-10-28 22:14:15 -0600

    se modifica numero wasap de navbar

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -6,7 +6,7 @@ export default function Header(){
         <>
         <div className="header-container">
             <nav>
-            <a href="https://wa.me/15551234567?text=Mira%20esta%20imagen:%20https%3A%2F%2Fcdn.lovcour.com%2Fwp-content%2Fuploads%2F2023%2F10%2F05182405%2F173-inch-waterproof-trendy-school-backpack-680x680.jpg">
+            <a href="https://wa.me/51910550230?text=Mira%20esta%20imagen:%20https%3A%2F%2Fcdn.lovcour.com%2Fwp-content%2Fuploads%2F2023%2F10%2F05182405%2F173-inch-waterproof-trendy-school-backpack-680x680.jpg">
     Compartir una imagen
 </a>
 </nav>
