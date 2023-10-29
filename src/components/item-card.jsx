188804ef10ab81154757c20ea957176cb10b1c5e
joshua <85379582+joshuaelliot@users.noreply.github.com>
2023-10-28 19:10:12 -0600

    se modifica texto url de wasap| se agrega link de imagen del json  de prueba

--- src/components/item-card.jsx
+++ src/components/item-card.jsx
@@ -29,7 +29,7 @@ const [popup,setPopup] = useState(false);
             <h3 className="description">{props.descriptionProduct}</h3>
             <h3 className="price-article">{`s/${props.price}`}</h3>
           </div>
-          <button className='btn-wasap'><a href={`https://wa.me/${number}?text=oyemegustas%20https://www.instagram.com/p/Co5noC3pPbe/?utm_source=ig_web_copy_link&igshid=MzRlODBiNWFlZA==`}>Pidelo aqui   <img
+          <button className='btn-wasap'><a href={`https://wa.me/${number}?text=oyemegustas%20https://cdn.lovcour.com/wp-content/uploads/2023/03/11075436/children-school-backpack-5-pcs-sets-17-inch-for-teens-girls-boys-43-680x680.jpg`}>Pidelo aqui   <img
             className='logo-wasap' src={LogoWasap} alt="" /></a></button>
         </div>
         {popup && (
