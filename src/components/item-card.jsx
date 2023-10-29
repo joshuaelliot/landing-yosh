e1c0d4bdc3cf4a89ded40d1350feee7f0fd984c2
joshua <85379582+joshuaelliot@users.noreply.github.com>
2023-10-28 19:36:27 -0600

    se modifica texto url de wasap| se agrega link de metadatos de prueba

--- src/components/item-card.jsx
+++ src/components/item-card.jsx
@@ -29,8 +29,15 @@ const [popup,setPopup] = useState(false);
             <h3 className="description">{props.descriptionProduct}</h3>
             <h3 className="price-article">{`s/${props.price}`}</h3>
           </div>
-          <button className='btn-wasap'><a href={`https://wa.me/${number}?text=oyemegustas%20https://cdn.lovcour.com/wp-content/uploads/2023/03/11075436/children-school-backpack-5-pcs-sets-17-inch-for-teens-girls-boys-43-680x680.jpg`}>Pidelo aqui   <img
-            className='logo-wasap' src={LogoWasap} alt="" /></a></button>
+          <button className='btn-wasap'><a href={`https://wa.me/${number}?text=Hola%20me%20interesa%20este%20producto%20${props.urlProduct}`}>
+            Pidelo aqui   <img className='logo-wasap' src={LogoWasap} alt="" /></a></button>
+            <a href="https://wa.me/15551234567?text=I'm%20interested%20in%20your%20car%20for%20sale">
+    <meta property="og:title" content="Título de la publicación de Instagram"/>
+    <meta property="og:description" content="Descripción de la publicación de Instagram"/>
+    <meta property="og:image" content="https://cdn.lovcour.com/wp-content/uploads/2023/10/05182405/173-inch-waterproof-trendy-school-backpack-680x680.jpg"/>
+    <meta property="og:url" content="URL de la publicación de Instagram"/> sad
+</a>
+
         </div>
         {popup && (
             <div className="show-poppup">
