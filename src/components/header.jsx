6fe2ef5faf2e7cdb5437151c5e14cd3973406f01
joshua <85379582+joshuaelliot@users.noreply.github.com>
2023-10-28 22:12:18 -0600

    se agrega banner nav | con un enlace wasap

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -5,6 +5,11 @@ export default function Header(){
     return (
         <>
         <div className="header-container">
+            <nav>
+            <a href="https://wa.me/15551234567?text=Mira%20esta%20imagen:%20https%3A%2F%2Fcdn.lovcour.com%2Fwp-content%2Fuploads%2F2023%2F10%2F05182405%2F173-inch-waterproof-trendy-school-backpack-680x680.jpg">
+    Compartir una imagen
+</a>
+</nav>
             <div className="writing-container">
                 <div className="filter-header">
                 <h1>Mochilas Escolares felices </h1>
